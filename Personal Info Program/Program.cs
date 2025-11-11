834e8cb60325c4834bccdc57979b0ddf477723cb
Tashenica Tulley <85678340+Tashenica@users.noreply.github.com>
2025-11-11 22:18:14 +0200

    Add project files.

---
+++ Personal Info Program/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more
+
+string name = "Tashenica";
+int age = 25;
+string city = "Cape Town";
+int currentYear = 2025;
+int yearBorn = currentYear - age;
+
+Console.WriteLine($"My name is {name.ToUpper()}, I am {age} years old and I stay in {city}.");
+Console.WriteLine($"I was born in the year {yearBorn}.");
